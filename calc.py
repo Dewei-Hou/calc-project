bb25542652be142ffab173315694d8508555d8fd
Dewei-Hou <a1075543@mail.nuk.edu.tw>
2026-05-22 10:56:25 +0800

    feat: add divide

--- calc.py
+++ calc.py
@@ -4,10 +4,11 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-def multiply(a, b):
-    return a * b
+def divide(a, b):
+    return a / b
 
 if __name__ == "__main__":
+    print("### My Calculator ###")
     print("add(2, 3) =", add(2, 3))
     print("subtract(5, 2) =", subtract(5, 2))
-    print("multiply(3, 4) =", multiply(3, 4))
+    print("divide(10, 2) =", divide(10, 2))
