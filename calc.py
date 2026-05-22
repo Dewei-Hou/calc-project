3d7e32b1e2e5ac05e362ac3386bc8fff07cef535
Dewei-Hou <a1075543@mail.nuk.edu.tw>
2026-05-22 10:54:29 +0800

    feat: add multiply and str

--- calc.py
+++ calc.py
@@ -8,6 +8,7 @@ def multiply(a, b):
     return a * b
 
 if __name__ == "__main__":
+    print("=== Calculator ===")
     print("add(2, 3) =", add(2, 3))
     print("subtract(5, 2) =", subtract(5, 2))
     print("multiply(3, 4) =", multiply(3, 4))
